7460be71ed003f02cd1ebb82c88718061ee6b704
Mayank-thite <milliondollar98@gmail.com>
2022-11-04 16:37:45 +0530

    add react

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import Navbar from "./components/Navbar";
+import Landing from "./components/Landing";
+
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+      <Landing />
+    </div>
+  );
+}
